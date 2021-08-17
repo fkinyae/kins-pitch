fc11bdf03a8845aab1b6b61a65a9851bc87750f9
Francis Kinyae <fmkinyae@gmail.com>
2021-08-17 15:25:44 +0300

    add db and secret key configs

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kinyae:kinyae@localhost/pitches'
 
 class ProdConfig(Config):
