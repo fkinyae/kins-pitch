252adb0cb6710658e1ddfe3b47f3a31872263cf5
Francis Kinyae <fmkinyae@gmail.com>
2021-08-18 15:59:47 +0300

    add a category forms class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User
+from ..models import User,Categories
 from wtforms import ValidationError
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.', validators=[Required()])
     submit = SubmitField('Submit')
+    
+class Categories(FlaskForm):
+    category = StringField('Category', validators=[Required()])
+    submit = SubmitField('submit')
+        
