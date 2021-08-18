b70f8f04335b4d42b0b14fb7017431a3bc293a68
Francis Kinyae <fmkinyae@gmail.com>
2021-08-18 10:23:03 +0300

    add user password class settings

--- app/models.py
+++ app/models.py
@@ -1,12 +1,21 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True,index=True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
     @property
     def password(self):
@@ -30,6 +39,15 @@ class Role(db.Model):
     
     def __repr__(self):
         return f'User {self.name}'   
+    
+class Categories(db.Model):
+    
+    __tablename__ = 'categories'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+            
     
 
     
