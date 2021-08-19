062aaee70e4c3b71b52cc46afaef1c382068434d
Francis Kinyae <fmkinyae@gmail.com>
2021-08-19 22:38:52 +0300

    add a pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,6 +4,12 @@ from wtforms.validators import Required,Email,EqualTo
 from ..models import User,Categories
 from wtforms import ValidationError
 
+class PitchForm(FlaskForm):
+    pitch = TextAreaField('Pitch')
+    submit = SubmitField('Submit')
+    
+
+
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.', validators=[Required()])
     submit = SubmitField('Submit')
