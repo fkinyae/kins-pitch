e6d221e2cd433c142be902beb6c69182cc01b720
Francis Kinyae <fmkinyae@gmail.com>
2021-08-23 16:42:40 +0300

    debug config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://kinyae:kinyae@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kinyae:kinyae@localhost/pitches'
     UPLOAD_PHOTOS_DEST ='app/static/photos'
     
     #email configurations
