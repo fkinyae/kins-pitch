edec4cc2e958d3ff9cca61d70beac7df5691fcff
Francis Kinyae <fmkinyae@gmail.com>
2021-08-26 12:55:35 +0300

    add online db

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-    DATABASE_URL = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
 
 class DevConfig(Config):
     DEBUG = True
